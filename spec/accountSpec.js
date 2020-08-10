e2cbe77dd5b020fb7371ebfaac2ca984fc542474
James Forster <jamesalexforster@gmail.com>
2020-08-10 14:19:15 +0100

    Account will now accept deposits

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,10 +1,20 @@
 describe("Account", function() {
 
+  beforeEach(function() {
+    account = new Account();
+  });
+
   describe("Account is instantiated with a default balance of 0", function() {
-    var account = new Account();
-    
     it('Will return 0 as its starting balance', function() {
       expect(account.balance).toEqual(0)
     });
+  });
+
+  describe("You can deposit funds into the account", function() {
+    it('Depositing money will increase the balance by the right amount', function() {
+      account.deposit(50);
+      expect(account.balance).toEqual(50)
+    })
+
   });
 });
